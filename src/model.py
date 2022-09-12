281a0cb1746f46bde0bafa08f950d687e5e96956
Sra Lin <srarod@gmail.com>
2022-09-12 13:24:34 +0800

    build backbone with efficient-b0

---
+++ src/model.py
@@ -0,0 +1,28 @@
+import os
+import torch
+import monai
+import torchinfo
+
+
+def get_backbone():
+    monai.networks.nets.efficientnet.efficientnet_params['efficientnet-b0'] = (1, 1, 28, 0.2, 0.2)
+    model = monai.networks.nets.EfficientNetBN('efficientnet-b0', 
+                                               spatial_dims = 2,
+                                               in_channels = 1,
+                                               num_classes = 14)
+    return model
+
+
+
+if __name__ == '__main__':
+    
+    model = get_backbone()
+    torchinfo.summary(model, input_size=(16,1,28,28))
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    test_input = torch.rand([16,1,28,28]).to(device)
+    test_output = model(test_input)
+    print(test_output.shape)
+    
+    

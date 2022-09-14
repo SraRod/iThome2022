1760e8eb7e2869ee53feb6092a34f1377c05aeae
Sra Lin <srarod@gmail.com>
2022-09-15 02:42:21 +0800

    update to run with config

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 import glob2
 import argparse
 import medmnist
@@ -8,30 +9,29 @@ from typing import Text
 from medmnist.info import INFO, DEFAULT_ROOT
 
 
-DATASET = 'chestmnist'
-TEMP = '/tmp'
-FOLDER = 'data'
-POSTFIX = 'png'
-OUTPUT_CSV = 'data/dataset.csv'
-
-
-def make_dataset() -> None:
+def make_dataset(config_path: Text) -> None:
     
-    medmnist_csv_file = os.path.join(FOLDER, DATASET + '.csv')
+    CONFIG = yaml.safe_load(open(config_path))['make_dataset']
+    medmnist_csv_file = os.path.join(CONFIG['save_folder'], CONFIG['dataset_from'] + '.csv')
     
     if not os.path.isfile(medmnist_csv_file):
         for split in ["train", "val", "test"]:
-            print(f"Saving {DATASET} {split}...")
-            dataset = getattr(medmnist, INFO[DATASET]['python_class'])(
-                split=split, root=TEMP, download=True)
-            dataset.save(FOLDER, POSTFIX)  
+            print(f"Saving {CONFIG['dataset_from']} {split}...")
+            dataset = getattr(medmnist, INFO[CONFIG['dataset_from']]['python_class'])(
+                split=split, root=CONFIG['tmp_folder'], download=True)
+            dataset.save(CONFIG['save_folder'], CONFIG['img_subfilename'])  
             
-    df = pd.read_csv('data/chestmnist.csv', header = None)
-    cols = ['split', 'img'] + list(INFO['chestmnist']['label'].values())
+    df = pd.read_csv(medmnist_csv_file, header = None)
+    cols = ['split', 'img'] + list(INFO[CONFIG['dataset_from']]['label'].values())
     df = df.rename(columns={ i: col for i, col in enumerate(cols)})
-    df['img'] = df['img'].apply(lambda x: os.path.join('data/chestmnist/', x))
+    df['img'] = df['img'].apply(lambda x: os.path.join(f"data/{CONFIG['dataset_from']}/", x))
     
-    df.to_csv(OUTPUT_CSV, index = False)
+    df.to_csv(CONFIG['dataset_file'], index = False)
 
 if __name__ == '__main__':
-    make_dataset()
+    
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config', dest='config', required=True)
+    args = args_parser.parse_args()
+
+    make_dataset(config_path=args.config)
--- src/model.py
+++ src/model.py
@@ -4,19 +4,22 @@ import monai
 import torchinfo
 
 
-def get_backbone():
-    monai.networks.nets.efficientnet.efficientnet_params['efficientnet-b0'] = (1, 1, 28, 0.2, 0.2)
-    model = monai.networks.nets.EfficientNetBN('efficientnet-b0', 
-                                               spatial_dims = 2,
-                                               in_channels = 1,
-                                               num_classes = 14)
-    return model
+def get_backbone(CONFIG):
+    if CONFIG['train']['backbone'] == 'efficientnet-b0':
+        monai.networks.nets.efficientnet.efficientnet_params['efficientnet-b0'] = (1, 1, 28, 0.2, 0.2)
+        model = monai.networks.nets.EfficientNetBN('efficientnet-b0', 
+                                                   spatial_dims = 2,
+                                                   in_channels = 1,
+                                                   num_classes = 14)
+        return model
+    else:
+        print('do not support other backbone until now!')
 
 
 
 if __name__ == '__main__':
     
-    model = get_backbone()
+    model = get_backbone({'train':{'backbone':'efficientnet-b0'}})
     torchinfo.summary(model, input_size=(16,1,28,28))
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")

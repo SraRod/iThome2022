25ef2105025897af02b9849fc321a9a3437cfc4b
Cheng Wei Lin <srarod@gmail.com>
2022-10-07 04:02:49 +0800

    47 day26 data augmentation (#48)
    * add data augmentation
    
    * add data aug sampling

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,6 +3,7 @@ import yaml
 import torch
 import monai
 import argparse
+import torchvision
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,7 +15,7 @@ SPLITS = ['TRAIN', 'VALIDATION', 'TEST']
 BATCH_SIZE = 16
 LABEL_LIST = ['atelectasis', 'cardiomegaly', 'effusion', 'infiltration', 'mass', 'nodule', 'pneumonia', 'pneumothorax', 'consolidation', 'edema', 'emphysema', 'fibrosis', 'pleural', 'hernia']
 
-def npz_dataset_to_dicts(npz_files, splits) -> Dict:
+def npz_dataset_to_dicts(npz_files: str, splits: str) -> Dict:
     split_mapping = {
         'TRAIN' :'train',
         'VALIDATION' : 'val',
@@ -35,7 +36,7 @@ def npz_dataset_to_dicts(npz_files, splits) -> Dict:
     return datasets
 
 
-def prepare_dataset(CONFIG) -> Dict:
+def prepare_dataset(CONFIG: Dict) -> Dict:
     
     dataset_source = os.path.splitext(CONFIG['preprocess']['dataset_file'])[-1]
     if dataset_source == '.csv':
@@ -47,7 +48,7 @@ def prepare_dataset(CONFIG) -> Dict:
         
     return datasets
 
-def prepare_transform(CONFIG) -> monai.transforms.transform:
+def prepare_transform(CONFIG: Dict) -> monai.transforms.transform:
     
     dataset_source = os.path.splitext(CONFIG['preprocess']['dataset_file'])[-1]
     if dataset_source == '.csv':
@@ -71,9 +72,33 @@ def prepare_transform(CONFIG) -> monai.transforms.transform:
     if CONFIG['preprocess']['input_channels'] != 1:
         transforms += [monai.transforms.RepeatChannelD(keys = ['img'], repeats = CONFIG['preprocess']['input_channels'])]
 
+    if CONFIG['preprocess']['imagenet_statistics']:
+        imagenet_statistics_convertor = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms += [monai.transforms.Lambdad(keys = ['img'], func = imagenet_statistics_convertor)]
+    
     transforms = monai.transforms.Compose(transforms)
     
     return transforms
+
+def prepare_data_aug(CONFIG: Dict) -> monai.transforms.transform:
+    
+    prob = CONFIG['train']['data_aug']['prob']
+    transforms_data_aug = [
+        monai.transforms.RandAffined(keys = ['img'], 
+                                     prob=prob, 
+                                     rotate_range = [-0.1, 0.1], 
+                                     translate_range = [-3, 3], 
+                                     scale_range = 0.1),
+        monai.transforms.RandGaussianNoised(keys = ['img'], 
+                                            prob=prob, 
+                                            mean=0.0, std=0.1),
+        monai.transforms.RandFlipd(keys = ['img'],
+                                   prob=prob, 
+                                   spatial_axis = [0])]
+    
+    transforms_data_aug = monai.transforms.Compose(transforms_data_aug)
+    
+    return transforms_data_aug
 
 
 if __name__ == '__main__':
@@ -92,6 +117,11 @@ if __name__ == '__main__':
         split : monai.data.Dataset(data = datasets[split], transform = transforms)    
         for split in SPLITS
     }
+    
+    if CONFIG['train']['data_aug']['use']:
+        transforms_data_aug = prepare_data_aug(CONFIG)
+        processed_datasets['TRAIN'] = monai.data.Dataset(data = processed_datasets['TRAIN'], transform = transforms_data_aug)    
+    
     data_generators = {
         split : torch.utils.data.DataLoader(processed_datasets[split],
                                             batch_size = BATCH_SIZE,
@@ -114,6 +144,27 @@ if __name__ == '__main__':
                 plt.imshow(batch['img'][i*4+j][0].T, 'gray', vmin = 0, vmax = 1)
                 plt.axis('off')
         plt.savefig(f'sampling of {split}.jpeg')
+        plt.close()
+        
+        
+    # sampling data augmentation from one sample
+    if CONFIG['train']['data_aug']['use']:
+        processed_dataset_da = monai.data.Dataset(data = datasets['TRAIN'][:1], transform = transforms) 
+        processed_dataset_da = monai.data.Dataset(data = processed_dataset_da, transform = transforms_data_aug)
+        data_generator_da = torch.utils.data.DataLoader(processed_dataset_da,
+                                            batch_size = 1,
+                                            shuffle = True,
+                                            collate_fn = monai.data.utils.pad_list_data_collate,
+                                            pin_memory=torch.cuda.is_available())
+        
+        plt.figure(figsize = (10, 10))
+        plt.suptitle(f'Data Augmentation Sampling', y = 0.92 ,fontsize=16)
+        for i in range(16):
+            plt.subplot(4,4,i+1)
+            for batch in data_generator_da:
+                plt.imshow(batch['img'][0][0].T, 'gray', vmin = 0, vmax = 1)
+            plt.axis('off')
+        plt.savefig(f'sampling of data augmentation.jpeg')
         plt.close()
         
             
